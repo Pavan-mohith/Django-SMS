43a6de781ea18f77e4b19accb1823a5ec034fdab
Pavan-mohith <pesalamohith@gmail.com>
2024-11-11 13:21:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hello:{name}')  # Press ⌘F8 to toggle the breakpoint.
+def roll_no(number):
+    print(f'RegNo:{number}')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('BALU\n')
+    roll_no('2300031585\n')
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
